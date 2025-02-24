f6ed820a260db32cc85cfa3d793e8f396f11fb64
nishchalatattve <nishchalatattve@tuta.com>
2025-02-24 14:33:05 +0000

    update

--- da_libraries/numpy_apis/element_wise_ops.py
+++ da_libraries/numpy_apis/element_wise_ops.py
@@ -21,3 +21,21 @@ b * e
 # %%
 (b * e).shape
 # %%
+# Let's write our own broadcast add function
+b = rng.random((10,))
+a = rng.random((32, 10))
+
+
+def broadcast_add(x, y):
+    assert x.ndim == 2
+    assert y.ndim == 1
+    assert x.shape[-1] == y.shape[0]
+
+    z = []
+    for i in range(x.shape[0]):
+        z.append(x[i] + y)
+    return np.array(z)
+
+
+c = broadcast_add(a, b)
+c
