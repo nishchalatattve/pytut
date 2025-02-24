e70ce200279477341f0dba2f992b500abbed1378
nishchalatattve <nishchalatattve@tuta.com>
2025-02-24 14:30:09 +0000

    init

---
+++ da_libraries/numpy_apis/element_wise_ops.py
@@ -0,0 +1,23 @@
+# %%
+import numpy as np
+
+rng = np.random.default_rng()
+# %%
+N = 200
+
+a = np.zeros((N,)),
+b = np.full((N,), 9)
+c = np.full((N,), 3)
+d = 7
+e = rng.random((N,))
+# %%
+b + d
+# %%
+b * c
+# %%
+b * d
+# %%
+b * e
+# %%
+(b * e).shape
+# %%
