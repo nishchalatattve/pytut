d25c6abc9624ac554e88aaafa9e09a4b84a1cd85
nishchalatattve <nishchalatattve@tuta.com>
2025-03-11 16:00:05 +0000

    update

--- raw_python/dictionary.py
+++ raw_python/dictionary.py
@@ -1,2 +1,10 @@
+# %%
+from datetime import datetime
 a = {"hi": 99, "pussy": 88, "adiyogi": 128}
 print(a.get("hii", '?'))
+# %%
+now = datetime.now()
+b = {"bhairavi": now.day == 11}
+b.get("bhairavi", '?')
+# %%
+# %%
