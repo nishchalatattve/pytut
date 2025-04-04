482e4db591444232f3571c4241641aa285d3d435
Hercule Poirot <136802495+nishchalatattve@users.noreply.github.com>
2025-04-04 11:59:09 +0100

    created notes

--- bash-notes.sh
+++ bash-notes.sh
@@ -1,6 +1,14 @@
 # !/bin/bash
 
 # git
+git config --list --show-origin
+git log --pretty=format:"%h-(%an, %cn): %s" -p --graph
+
 # git-lfs
 git lfs install
-git lfs track "*.psd"
+git lfs track "*.pdf"
+git lfs track "*.jpg"
+git lfs track "*.jpeg"
+git lfs track "*.png"
+git lfs track "*.otf"
+git lfs track "*.ttf"
